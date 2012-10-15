bf9c1b223ed804da75159596ad13e8ac6c0364d4
Frederiko Costa <frederiko@gmail.com>
2012-10-15 10:57:06 -0700

    apache config dir customized per distro
    missing name on file resource

--- apache/manifests/params.pp
+++ apache/manifests/params.pp
@@ -3,10 +3,12 @@ class apache::params {
 		'Ubuntu','Debian': { 
 		  $pkg='apache2'
 		  $svc="apache"
+		  $cfg_dir="/etc/apache2/sites-enabled"
 		  }
 		'RedHat', 'CentOS': { 
 		  $pkg='httpd' 
 		  $svc='httpd'
+		  $cfg_dir="/etc/httpd/conf.d"
 		  }
 	}
 }
--- apache/manifests/vhost.pp
+++ apache/manifests/vhost.pp
@@ -2,17 +2,15 @@
 # creating/modifying an Apache virtual host entry
 # each host can have as many as necessary
 
-define apache::vhost($port, $docroot, $ssl=true, $template='apache/vhost.conf.erb',
-$priority, $serveraliases= '') {
+define apache::vhost($ip, $port, $docroot, $ssl=true, $template='apache/vhost.conf.erb',
+$priority, $serveraliases= '', $logdir='/var/log/apache') {
   include apache
   
-  file { $apache::params::cfg_dir: 
+  file { "$apache::params::cfg_dir/${priority}-${name}": 
     content => template($template)
     owner => root,
     group => root,
     mode => 777,
     require => Class["apache::install"],
   }
-    
-  
 }
