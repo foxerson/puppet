42cd13a66ed089b06ee796af8f3c46dd0078d662
Frederiko Costa <frederiko@gmail.com>
2012-10-17 10:42:38 -0700

    added vhost template for vanity URLs

--- apache/manifests/vhost.pp
+++ apache/manifests/vhost.pp
@@ -2,7 +2,22 @@
 # creating/modifying an Apache virtual host entry
 # each host can have as many as necessary
 
+# Virtual Host definition for basic website, either using SSL or not.
 define apache::vhost($ip, $docroot, $ssl=true, $template='apache/vhost.conf.erb',
+$priority, $serveraliases= '', $logdir='/var/log/apache') {
+  include apache
+  
+  file { "$apache::params::cfg_dir/${priority}-${name}": 
+    content => template($template),
+    owner => root,
+    group => root,
+    mode => 777,
+    require => Class["apache::install"],
+  }
+}
+
+# virtual host definition for vanity URL
+define apache::vhost_redir($ip, $ssl=false, $dest_url, $template='apache/vhost_redirect.conf.erb',
 $priority, $serveraliases= '', $logdir='/var/log/apache') {
   include apache
   
