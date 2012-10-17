1da30ff17af7d6e685f39a580a92deecd5ebf4ff
Frederiko Costa <frederiko@gmail.com>
2012-10-17 15:04:11 -0700

    vhost_proxy configuration and template

--- apache/manifests/vhost.pp
+++ apache/manifests/vhost.pp
@@ -29,3 +29,37 @@ $priority, $serveraliases= '', $logdir='/var/log/apache') {
     require => Class["apache::install"],
   }
 }
+
+# generating a configuration with multiple locations and webauth
+# Sample call:
+# apache::vhost_proxy { 'lbre-basemap.stanford.edu':
+#   ip => '171.64.144.200',
+#   serveraliases => ['lbre-basemap', 'basemap'],
+#   location => [ {
+#                  "Location"=> "/mapguide2011",
+#                  "AuthType" => "WebAuth",
+#                  "Require privgroup" => [ 'stanford:faculty', 'stanford:staff', 'stanford:group' ],
+#                  "ProxyPass" => 'http://171.64.144.91/mapguide2011/',
+#                  "ProxyPassReverse" => 'http://171.64.144.91/mapguide2011/',
+#                  },
+#                  {
+#                    "Location"=> "/",
+#                    "AuthType" => "WebAuth",
+#                    "Require privgroup" => [ 'stanford:faculty' ],
+#                    "ProxyPass" => 'http://171.64.144.91/',
+#                    "ProxyPassReverse" => 'http://171.64.144.91/',
+#                  }
+#               ],
+# }
+
+define apache::vhost_proxy($ip, $serveraliases, $template='apache/vhost_redirect.conf.erb', *$location }
+  include apache
+  
+  file { "$apache::params::cfg_dir/${priority}-${name}": 
+    content => template($template),
+    owner => root,
+    group => root,
+    mode => 777,
+    require => Class["apache::install"],
+  }
+}
