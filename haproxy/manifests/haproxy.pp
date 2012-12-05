9ff33468182a807ae9e12344fed5f66ada1321f7
Frederiko Costa <frederiko@gmail.com>
2012-12-05 14:20:20 -0800

    initial untested haproxy module

---
+++ haproxy/manifests/haproxy.pp
@@ -0,0 +1,25 @@
+# this is how the entry will be, ex:
+# listen webfarm 80
+
+
+# accepted backends format:
+# [ "192.168.1.2:80"]
+# "192.168.1.2:80","192.168.1.3:8080"
+# [ "192.168.1.2:80", 
+    "192.168.1.4:8080"]
+
+define($name, $port=8080, $backends, $template='haproxy/haproxy.conf.erb',
+$cookie=false, $check=true) {
+  
+  include haproxy
+  
+  file { "${name}":
+    name => $operatingsystem ? {
+            'debian' => "/etc/haproxy/${cfgfile}",
+            'ubuntu' => "/etc/haproxy/${cfgfile}",
+            'redhat' => "/etc/haproxy/${cfgfile}",
+            },
+    source => template($template),
+    require => Package['haproxy'],
+  } 
+}
---
+++ haproxy/manifests/init.pp
@@ -0,0 +1,15 @@
+class haproxy {
+  
+  # same namevar for both RH and Ubuntu.
+  package { 'haproxy':
+    ensure => installed, 
+  }
+  
+  service { 'haproxy':
+    ensure => 'running',
+    hasstatus => true,
+    hasrestart => true,
+    enable => true;
+    require => Package['haproxy'],
+  }
+}
