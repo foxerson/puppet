37b99939c8b29cbabaff343b6225554d9df6cdb9
Frederiko Costa <frederiko@gmail.com>
2012-12-06 09:51:31 -0800

    missing :

--- haproxy/manifests/config.pp
+++ haproxy/manifests/config.pp
@@ -1,19 +1,9 @@
-# this is how the entry will be, ex:
-# listen webfarm 80
-
-
-# accepted backends format:
-# [ "192.168.1.2:80"]
-# "192.168.1.2:80","192.168.1.3:8080"
-# [ "192.168.1.2:80", 
-#    "192.168.1.4:8080"]
-
 define haproxy::config ($port=8080, $backends, $template='haproxy/haproxy.conf.erb',
 $cookie=false, $checksvr=true) {
   
   include haproxy
 
-  file { "${name}.conf"
+  file { "${name}.conf":
     content => template($template),
     require => Package['haproxy'],
   } 
