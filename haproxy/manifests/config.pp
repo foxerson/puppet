3e474c7abbb8cdd7b64a9eca39a6af3db5fe0093
Frederiko Costa <frederiko@gmail.com>
2012-12-06 09:48:32 -0800

    fixed source parameter. Changed to content

--- haproxy/manifests/config.pp
+++ haproxy/manifests/config.pp
@@ -6,20 +6,15 @@
 # [ "192.168.1.2:80"]
 # "192.168.1.2:80","192.168.1.3:8080"
 # [ "192.168.1.2:80", 
-    "192.168.1.4:8080"]
+#    "192.168.1.4:8080"]
 
-define haproxy::config ($name, $port=8080, $backends, $template='haproxy/haproxy.conf.erb',
-$cookie=false, $check=true) {
+define haproxy::config ($port=8080, $backends, $template='haproxy/haproxy.conf.erb',
+$cookie=false, $checksvr=true) {
   
   include haproxy
-  
-  file { "${name}":
-    name => $operatingsystem ? {
-            'debian' => "/etc/haproxy/${cfgfile}",
-            'ubuntu' => "/etc/haproxy/${cfgfile}",
-            'redhat' => "/etc/haproxy/${cfgfile}",
-            },
-    source => template($template),
+
+  file { "${name}.conf"
+    content => template($template),
     require => Package['haproxy'],
   } 
 }
