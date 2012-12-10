0401a6af095cf77a3b42d20b2cce2b79329ab075
Frederiko Costa <frederiko@gmail.com>
2012-12-10 13:25:15 -0800

    webauth module used on ITS

---
+++ webauth/manifests/init.pp
@@ -0,0 +1,27 @@
+# A WebAuth client.  Note that this probably won't make sense to include on a
+# host that doesn't also include the apache module.
+
+class webauth {
+  package { 'stanford-webauth': ensure => present }
+
+  # On Debian squeeze, prefer the version from backports.
+  if ($lsbdistcodename == 'squeeze') {
+    file { '/etc/apt/preferences.d/webauth':
+      source => 'puppet:///modules/webauth/etc/apt/preferences.d/webauth',
+      before => Package['stanford-webauth'],
+    }
+  }
+
+  # Automate the keytab installation.
+  file { '/etc/webauth': ensure => directory }
+  wallet { "webauth/${fqdn}":
+    path    => '/etc/webauth/keytab',
+    mode    => '0640',
+    owner   => 'root',
+    group   => $operatingsystem ? {
+            'redhat' => 'apache',
+            default  => 'www-data', 
+        },
+    require => Package['stanford-webauth'],
+  }
+}
