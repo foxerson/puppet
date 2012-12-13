bc8b387409142f75edd80627394b0d7100b1f443
Frederiko Costa <frederiko@gmail.com>
2012-12-13 10:08:32 -0800

    wallet client adds stanford repository on Ubuntu

--- wallet/manifests/client.pp
+++ wallet/manifests/client.pp
@@ -6,22 +6,30 @@
 
 class wallet::client {
   
-  # Required for wallet-client
-  file { "/etc/apt/sources.list.d/stanford.list":
-    ensure => present,
-    owner => "root",
-    group => "root",
-    mode => 0644,
-    content => "deb http://debian.stanford.edu/debian-stanford stable main",	  
-  }
+  if $operatingsystem == "Ubuntu" or $operatingsystem =="debian" {
+      # Required for wallet-client
+      file { "/etc/apt/sources.list.d/stanford.list":
+        ensure => present,
+        owner => "root",
+        group => "root",
+        mode => 0644,
+        content => "deb http://debian.stanford.edu/debian-stanford stable main",	  
+      }
   
-  # It's OK to install unsigned packages
-  file { "/etc/apt/apt.conf.d/99auth":       
-     owner     => root,
-     group     => root,
-     content   => "APT::Get::AllowUnauthenticated yes;",
-     mode      => 644,
-     subscribe => File["/etc/apt/sources.list.d/stanford.list"],
+      # It's OK to install unsigned packages
+      file { "/etc/apt/apt.conf.d/99auth":       
+         owner     => root,
+         group     => root,
+         content   => "APT::Get::AllowUnauthenticated yes;",
+         mode      => 644,
+         subscribe => File["/etc/apt/sources.list.d/stanford.list"],
+         notify => Exec["aptitude update"],
+      }
+  
+      exec { "aptitude update":
+        path => "/usr/bin",
+        command => "aptitude update",
+      }
   }
   
   case $lsbdistrelease {
