219dc918b4084a2a438134bfa68ba15afc3156a7
Frederiko Costa <frederiko@gmail.com>
2012-12-14 10:36:19 -0800

    webauth module installation no longer relies on stanford-webauth package

--- webauth/manifests/init.pp
+++ webauth/manifests/init.pp
@@ -1,8 +1,20 @@
 # A WebAuth client.  Note that this probably won't make sense to include on a
 # host that doesn't also include the apache module.
 
-class webauth {  
-  package { 'stanford-webauth': 
+class webauth {
+  
+ 	case $operatingsystem {
+		'Ubuntu','Debian': { 
+		  $pkg='libapache2-webauth'
+		  $cfg="/etc/apache2/conf.d/stanford-webauth"
+		  }
+		'RedHat', 'CentOS': { 
+		  $pkg='webauth' 
+		  $cfg="/etc/httpd/conf.d/stanford-webauth.conf"
+		  }
+	}
+ 
+  package { $pkg: 
     ensure => present,
    }
 
@@ -10,10 +22,17 @@ class webauth {
   if ($lsbdistcodename == 'squeeze') {
     file { '/etc/apt/preferences.d/webauth':
       source => 'puppet:///modules/webauth/etc/apt/preferences.d/webauth',
-      before => Package['stanford-webauth'],
+      before => Package["${pkg}"],
     }
   }
-
+  
+  # dont use stanford-webauth packages.
+  file { $cfg:
+    source => 'puppet:///modules/webauth/webauth.conf',
+    ensure => present,
+    before => Package["${pkg}"],
+  }
+    
   # Automate the keytab installation.
   file { '/etc/webauth': ensure => directory }
   wallet { "webauth/${fqdn}":
@@ -24,6 +43,6 @@ class webauth {
             'redhat' => 'apache',
             default  => 'www-data', 
         },
-    require => Package['stanford-webauth'],
+    require => Package["${pkg}"],
   }
 }
