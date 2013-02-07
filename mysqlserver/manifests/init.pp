ac20b9a56dc867d7ac3755caab9328ea27a78bff
root <root@lbre-db02.Stanford.EDU>
2013-02-07 14:01:37 -0800

    General cleanup on the mysql code and adding configuration classes for master and slave (plus a common subset)

--- mysqlserver/manifests/init.pp
+++ mysqlserver/manifests/init.pp
@@ -1,4 +1,10 @@
 # installs and configure mysql server on Ubuntu hosts
-class mysqlserver {
-  include mysqlserver::install, mysqlserver::service
+# NOTE: Ubuntu hosts ***ONLY***
+
+class mysqlserver-master {
+	include mysqlserver::install, mysqlserver::service, mysqlserver::config-master
+}
+
+class mysqlserver-slave {
+	include mysqlserver::install, mysqlserver::service, mysqlserver::config-slave
 }
