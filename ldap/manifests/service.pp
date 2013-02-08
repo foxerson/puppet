e32ffc66f2291999aa2fe7c032c7019c60e991fa
Frederiko Costa <frederiko@gmail.com>
2013-02-08 13:42:28 -0800

    start both nscd and nslcd services

--- ldap/manifests/service.pp
+++ ldap/manifests/service.pp
@@ -1,4 +1,12 @@
 class ldap::service {
+  service { "nscd":
+    ensure => running,
+    hasstatus => true,
+    hasrestart => true,
+    enable => true,
+    require => Class["ldap::install"], 
+  }
+
   service { "nslcd":
     ensure => running,
     hasstatus => true,
