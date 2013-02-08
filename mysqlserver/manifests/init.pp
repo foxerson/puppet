e1cb60a5372112b867c5e21f51802b79cb0940dd
marcelom <marcelom@stanford.edu>
2013-02-08 15:32:04 -0800

    forgot an important comment...

--- mysqlserver/manifests/init.pp
+++ mysqlserver/manifests/init.pp
@@ -1,6 +1,9 @@
 # installs and configure mysql server on Ubuntu hosts
 # NOTE: Ubuntu hosts ***ONLY***
 
+# To try this localy, use it like this:
+#     puppet apply -e "class {'mysqlserver': old_root_pw=>'oldpass', root_pw=>'newpass',}" init.pp --verbose
+
 class mysqlserver ( $old_root_pw='', $root_pw='UNSET', $mode='UNSET' ) {
 	include mysqlserver::install, mysqlserver::service
 
